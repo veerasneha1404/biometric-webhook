15311e918b52ea19ee9880c48338aca2f0c39955
veerasneha1404 <veerasneha1404@gmail.com>
2026-02-09 16:48:24 +0530

    support root webhook

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const supabase = createClient(
 );
 
 // Webhook endpoint
-app.post("/webhook", async (req, res) => {
+app.post(["/", "/webhook"], async (req, res) => {
   try {
     const data = req.body;
 
